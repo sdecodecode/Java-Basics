cd0245a2807779f478efb052d3e84f01f52d906b
Devanshu <captaindevil108@gmail.com>
2023-03-09 20:40:37 +0530

    Question 4

---
+++ DivisorDivident.java
@@ -0,0 +1,12 @@
+// 4) Write a program where the user is asked to enter two integers (divisor and dividend) and the quotient and the remainder of their division is computed.(Both divisor and dividend should be integers.)
+
+import java.util.Scanner;
+
+public class DivisorDivident {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+
+  scanner.close();
+  }
+}
